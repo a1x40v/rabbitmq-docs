1ff7f348b6ca8c3b53aca54163d1257cb8174916
a1x40v <a1x40v@gmail.com>
2023-06-09 16:45:02 +0300

    Lesson 3. Publish Subscribe

---
+++ 3-publish-subscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+/*
+    Exchange типа fanout рассылает сообщение во все queue, которые знает.
+    (т.е. которые привязаны к нему - binding)
+*/
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+/*  В отличие от предыдущих примеров, не указываем имя конкретной queue */
+channel.BasicPublish(exchange: "logs",
+                     routingKey: string.Empty,
+                     basicProperties: null,
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
---
+++ 3-publish-subscribe/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+/* В отличие от предыдущих примеров, не указываем имя конкретной queue
+
+Firstly, whenever we connect to Rabbit we need a fresh, empty queue with generated name.
+Secondly, once we disconnect the consumer the queue should be automatically deleted.
+
+In the .NET client, when we supply no parameters to QueueDeclare() we create a non-durable, 
+exclusive, autodelete queue with a generated name
+*/
+var queueName = channel.QueueDeclare().QueueName;
+
+
+/*  Указываем queue принимать сообщения от указанного exchange (binding) */
+channel.QueueBind(queue: queueName,
+                  exchange: "logs",
+                  routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+};
+
+channel.BasicConsume(queue: queueName,
+                     autoAck: true,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
